3ac5fe9110e0c73d78a55013cba0fc4ba98d0c37
VRAJ SONI <vrajjsoni70419@gmail.com>
2026-06-25 14:23:34 +0530

    pr1_2 Commit

---
+++ src/p1_2.kt
@@ -0,0 +1,17 @@
+fun main(){
+    val i = 30000
+    val j = i.toDouble()
+    val s1 = "30500"
+    val k = s1.toInt()
+    val s2 = "1023546.4567689789"
+    val k2 = s2.toDouble()
+    val k3 = k2.toInt()
+
+    println("i = $i")
+    println("j = $j")
+    println("s1 = $s1")
+    println("k = $k")
+    println("k2 = $k2")
+    println("s2 = $s2")
+    println("k3 = $k3")
+}
