1485ca411d59ddd7a253f51310ef501e734c753c
VRAJ SONI <vrajjsoni70419@gmail.com>
2026-06-25 14:10:29 +0530

    Initial commit

---
+++ src/p1_1.kt
@@ -0,0 +1,31 @@
+/**
+ * You can edit, run, and share this code.
+ * play.kotlinlang.org
+ */
+fun main() {
+    var i = 200
+    var j : Int = 100
+    var f : Float = 10.647f
+    var c : Char = 'b'
+    var s : String = "Hello World"
+    var bol : Boolean = true
+    var st : Short = 32767
+    var db : Double = 10.648798765416
+    var bt : Byte = 25
+    var lg : Long = 100000000L
+
+    val z = 20000
+
+    println("i= "+i)
+    println("j= "+j)
+    println("f= "+f)
+    println("c= "+c)
+    println("s= "+s)
+    println("bol= "+bol)
+    println("st= "+st)
+    println("db= "+db)
+    println("bt= "+bt)
+    println("Lg= "+lg)
+
+    println("z= "+z)
+}
